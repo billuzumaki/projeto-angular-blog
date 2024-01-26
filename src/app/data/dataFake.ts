0e2b8ba7291831ff7b94a1bd92a257d85530dcdd
billuzumaki <santos-emiliano@hotmail.com>
2024-01-25 21:36:40 -0300

    second commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Projetos Pessoais",
+    "description": "projeto pessoais criados para aprender programação",
+    "photoCover":"https://files.tecnoblog.net/wp-content/uploads/2022/10/github-simbolo.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Todo-list",
+    "description": "projeto criado para fazer uma lista de caixas colhidas com a possibilidade de saber o total de caixas colhidas",
+    "photoCover":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw-7V_6hE5k9XmPaD2rO3RLhRL-GDK_07tHwxyQIWZI3tkxwFJJ4SnZ9tTpAKqw1Th2rY&usqp=CAU"
+  },
+  {
+    "id":"3",
+    "title": "Calculadora de Juros compostos",
+    "description": "projeto de uma calculadora simples de juros compostos para saber quanto de investimento terei em um determinado tempo",
+    "photoCover": "https://ici-s.umn.edu/files/caJix4jhH_/calculator-research-1?preferredLocale=en-US"
+  }
+]
